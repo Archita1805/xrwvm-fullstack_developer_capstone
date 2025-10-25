9048d4215ef2c87ee4bba504f1f0ecf57d9d4d43
Archita1805 <architadhepe05@gmail.com>
2025-10-25 10:27:11 -0400

    Updated files in network skills lab

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -23,8 +23,10 @@ SECRET_KEY =\
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['localhost','https://architadhepe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = ['https://architadhepe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = ['architadhepe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'localhost',
+    '127.0.0.1',]
+CSRF_TRUSTED_ORIGINS = ['https://architadhepe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
 
 REST_FRAMEWORK = {
@@ -63,7 +65,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+            os.path.join(BASE_DIR,'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -156,5 +160,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
